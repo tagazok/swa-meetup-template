ac69aa350b365df89c77816971b6c15d5d8193e8
Olivier Leplus <leplus.olivier@gmail.com>
2021-12-10 11:36:51 +0000

    feat: demo

--- api/tasks-get/index.js
+++ api/tasks-get/index.js
@@ -1,14 +1,32 @@
 const {connectDb, getUser} = require('../shared/shared');
 
 module.exports = async function (context, req) {
-    const user = getUser(req);
+    // const user = getUser(req);
 
-    const database = await connectDb();
+    // const database = await connectDb();
 
-    const response = await database.collection("tasks").find({
-        userId: user.userId
-    });
-    const tasks = await response.toArray();
+    // const response = await database.collection("tasks").find({
+    //     userId: user.userId
+    // });
+    // const tasks = await response.toArray();
+
+    const tasks = [
+        {
+            id: 1,
+            label: "🍔 Eat",
+            status: ""
+        },
+        {
+            id: 2,
+            label: "🛏 Sleep",
+            status: ""
+        },
+        {
+            id: 3,
+            label: "</> Code",
+            status: ""
+        }
+    ];
 
     context.res = {
         body: tasks
